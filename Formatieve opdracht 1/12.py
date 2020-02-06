45ec040bf82a63460981c0bb8a06b4b7db7ff523
Floris Videler <floris.videler@student.hu.nl>
2020-02-06 10:17:18 +0100

    12 gemaakt

---
+++ Formatieve opdracht 1/12.py
@@ -0,0 +1,13 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        if i%3 == 0 and i%5 == 0:
+            output = "fizzbuzz"
+        elif i%3 == 0:
+            output = "fizz"
+        elif i%5 == 0:
+            output = "buz"
+        else:
+            output = i
+        print(output)
+
+fizzbuzz()
