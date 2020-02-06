d40da7c6b4a1e1f32ad0b66c7e0bd4e6708bce4e
Floris Videler <floris.videler@student.hu.nl>
2020-02-06 10:37:46 +0100

    .

--- Formatieve opdracht 1/10.py
+++ Formatieve opdracht 1/10.py
@@ -1,8 +1,8 @@
 def fiboncci(n, a):
-    print(a, n)
     if n <= 1:
         return n
     else:
         return fiboncci(n - 1, 1) + fiboncci(n - 2, 2)
+
 
 print(fiboncci(6, 0))
--- Formatieve opdracht 1/12.py
+++ Formatieve opdracht 1/12.py
@@ -1,13 +1,14 @@
 def fizzbuzz():
     for i in range(1, 101):
-        if i%3 == 0 and i%5 == 0:
+        if i % 3 == 0 and i % 5 == 0:
             output = "fizzbuzz"
-        elif i%3 == 0:
+        elif i % 3 == 0:
             output = "fizz"
-        elif i%5 == 0:
+        elif i % 5 == 0:
             output = "buz"
         else:
             output = i
         print(output)
+
 
 fizzbuzz()
