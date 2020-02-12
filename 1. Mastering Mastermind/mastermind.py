f65f76e3a1c4431c7ea590c861314877fdf7bc04
Floris Videler <floris.videler@student.hu.nl>
2020-02-12 10:23:39 +0100

    tmp

--- 1. Mastering Mastermind/mastermind.py
+++ 1. Mastering Mastermind/mastermind.py
@@ -1,3 +1,35 @@
+import random
+
+colors = [
+    {
+        "Kleur": "Groen",
+        "Afkorting": "G"
+    },
+    {
+        "Kleur": "Rood",
+        "Afkorting": "R"
+    },
+    {
+        "Kleur": "Blauw",
+        "Afkorting": "B"
+    },
+    {
+        "Kleur": "Oranje",
+        "Afkorting": "O"
+    },
+    {
+        "Kleur": "Wit",
+        "Afkorting": "W"
+    },
+    {
+        "Kleur": "Paars",
+        "Afkorting": "P"
+    }
+]
+
+code_global = []
+
+
 def menu():
     print("Welkom to mastermind!")
     gives_feedback = "n"
@@ -6,8 +38,37 @@ def menu():
         if who_is_playing.lower() == "y":
             break
         elif who_is_playing.lower() == "n":
-            gives_feedback = input("Geeft de pc feedback? (y/n) ")
-            if gives_feedback.
+            while True:
+                gives_feedback = input("Geeft de pc feedback? (y/n) ")
+                if gives_feedback.lower() == "y" or gives_feedback.lower() == "n":
+                    break
+            break
+    return who_is_playing, gives_feedback
 
 
-menu()
+def pc_game():
+    return
+
+
+def player_game():
+    # Set code
+    code = random.sample(colors, k=4)
+    set_code(code)
+    print(code)
+
+
+def set_code(code):
+    code_global.clear()
+    for i in code:
+        code_global.append(i["Afkorting"])
+    print(code_global)
+
+
+
+def game():
+    settings = menu()
+    if settings[0] == "y":
+        player_game()
+
+
+game()
