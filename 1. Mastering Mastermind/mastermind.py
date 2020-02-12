01d6024a06a31483135b5cd109581aafb17cd86b
Floris Videler <floris.videler@student.hu.nl>
2020-02-12 14:29:28 +0100

    First playable version!

--- 1. Mastering Mastermind/mastermind.py
+++ 1. Mastering Mastermind/mastermind.py
@@ -51,16 +51,53 @@ def pc_game():
 
 
 def player_game():
+    board = ""
+    steps = 0
     # Set code
     code = random.sample(colors, k=4)
     set_code(code)
-    print(code)
+    while True:
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n=========================================================")
+        print(board)
+        guess_input = input("Do een gok: ")
+        try:
+            guess_list = guess_input.upper().replace(" ", "").split(",")
+            #print(guess_list)
+            guess_response = auto_feedback(guess_list)
+            steps += 1
+            right_color_str = ""
+            if guess_response[0] > 0:
+                right_color_str = "?" * guess_response[0]
+            right_place_str = ""
+            if guess_response[1] > 0:
+                right_place_str = "!" * guess_response[1]
+            board += "{:^10} {:^10} {:^10} {:^10} || {:^10} {:^10}\n".format(guess_list[0], guess_list[1], guess_list[2], guess_list[3], right_color_str, right_place_str)
+            if guess_response[1] == 4:
+                print(f"WINNER WINNER CHICKEN DINNER.\nJE HEBT DE CODE GERADEN IN {steps} STAPPEN!")
+                break
+            #print(guess_response)
+        except:
+            print("GEEN GELDIGE INPUT!")
+
+
+def auto_feedback(guesslst):
+    #Check if color is in code
+    right_color = 0
+    right_place = 0
+    for g, color in enumerate(guesslst):
+        if color in code_global:
+            if code_global[g] == guesslst[g]:
+                right_place += 1
+            else:
+                right_color += 1
+    return right_color, right_place
 
 
 def set_code(code):
     code_global.clear()
     for i in code:
         code_global.append(i["Afkorting"])
+    print("Code is set!")
     print(code_global)
 
 
