8d10d2a1f61d78e40e0994e3b72bbc13986adde4
David Demmers <daviddemmers@gmail.com>
2020-02-18 10:44:33 +0100

    Code review

--- 1. Mastering Mastermind/mastermind.py
+++ 1. Mastering Mastermind/mastermind.py
@@ -1,4 +1,5 @@
-import random, ast
+import random
+import ast
 import mastermindalgo
 import feedback as fb
 
@@ -11,6 +12,7 @@ def menu():
     print("Welkom to mastermind!")
     gives_feedback = "n"
     pcstrat = "n"
+    # Ipv het breaken en altijd true kan je checken of er iets in je return staat
     while True:
         who_is_playing = input("Raadt de speler? (y/n) ")
         if who_is_playing.lower() == "y":
@@ -61,30 +63,15 @@ def player_game():
                 break
         except Exception as ex:
             print("GEEN GELDIGE INPUT!")
-        # try:
-        #     guess_list = guess_input.upper().replace(" ", "").split(",")
-        #     # print(guess_list)
-        #     guess_response = auto_feedback(guess_list, code)
-        #     steps += 1
-        #     right_color_str = ""
-        #     if guess_response[0] > 0:
-        #         right_color_str = "?" * guess_response[0]
-        #     right_place_str = ""
-        #     if guess_response[1] > 0:
-        #         right_place_str = "!" * guess_response[1]
-        #     board += "{:^10} {:^10} {:^10} {:^10} || {:^10} {:^10}\n".format(guess_list[0], guess_list[1],
-        #                                                                      guess_list[2], guess_list[3],
-        #                                                                      right_place_str, right_color_str)
-        #     if guess_response[1] == 4:
-        #         print(f"WINNER WINNER CHICKEN DINNER.\nJE HEBT DE CODE GERADEN IN {steps} STAPPEN!")
-        #         break
-        #     # print(guess_response)
-        # except Exception as ex:
-        #     print("GEEN GELDIGE INPUT!")
-        #     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-        #     message = template.format(type(ex).__name__, ex.args)
-        #     print(message)
 
+def generate_all_codes():
+    lst = []
+    for a in colors:
+        for b in colors:
+            for c in colors:
+                for d in colors:
+                    lst.append([a, b, c, d])
+    return lst
 
 def pc_game(feedback, strat):
     manual_feedback = False
@@ -92,34 +79,31 @@ def pc_game(feedback, strat):
         manual_feedback = True
     board = ""
     steps = 0
-    code = input("Verzin een code: ").upper().replace(" ", "").split(",")
-    plausible_codes = []
-    for a in colors:
-        for b in colors:
-            for c in colors:
-                for d in colors:
-                    plausible_codes.append([a, b, c, d])
+    #You only use this once but a sanitisation function might be usefull if you ask for more user input in this format
+    code = input("Verzin een code: ").upper().replace(" ", "").split(",") 
+    plausible_codes = generate_all_codes()
+    print(plausible_codes)
     n = 0
     feedback = 0, 0
     while True:
         print(board)
         if strat == "simple":
             if n > 0:
-                plausible_codes = mastermindalgo.simple_algortime(plausible_codes, feedback, guess_list)
+                plausible_codes = mastermindalgo.simple_algorithm(plausible_codes, feedback, guess_list)
             else:
                 n += 1
             guess_list = random.choice(plausible_codes)
         elif strat == "worstcase":
             if n > 0:
-                plausible_codes = mastermindalgo.simple_algortime(plausible_codes, feedback, guess_list)
+                plausible_codes = mastermindalgo.simple_algorithm(plausible_codes, feedback, guess_list)
             else:
                 n += 1
                 # Best guess based on expected size
-            guess_list = mastermindalgo.best_worstcase_algortime(plausible_codes)
+            guess_list = mastermindalgo.best_worstcase_algorithm(plausible_codes)
 
         elif strat == "zelfbedacht":
             if n > 0:
-                plausible_codes = mastermindalgo.selfmade_algortime(plausible_codes, feedback, guess_list)
+                plausible_codes = mastermindalgo.selfmade_algorithm(plausible_codes, feedback, guess_list)
             else:
                 n += 1
                 # Best guess based on expected size
@@ -129,24 +113,24 @@ def pc_game(feedback, strat):
             guess_list = mastermindalgo.random_guess(colors)
 
         if manual_feedback:
-            guess_response = fb.manual_feedback(guess_list, code)
-            feedback = guess_response
+            right_color, right_place = fb.manual_feedback(guess_list, code)
+            feedback = [right_color, right_place]
         else:
-            guess_response = fb.auto_feedback(guess_list, code)
-            feedback = guess_response
+            right_color, right_place = fb.auto_feedback(guess_list, code)
+            feedback = [right_color, right_place]
 
         steps += 1
         right_color_str = ""
-        if guess_response[0] > 0:
-            right_color_str = "?" * guess_response[0]
+        if right_color > 0:
+            right_color_str = "?" * right_color
         right_place_str = ""
-        if guess_response[1] > 0:
-            right_place_str = "!" * guess_response[1]
+        if right_place > 0:
+            right_place_str = "!" * right_place
         print(guess_list)
         board += "{:^10} {:^10} {:^10} {:^10} || {:^10} {:^10}\n".format(guess_list[0], guess_list[1],
                                                                          guess_list[2], guess_list[3],
                                                                          right_place_str, right_color_str)
-        if guess_response[1] == 4:
+        if right_place == 4:
             print(board)
             print(f"WINNER WINNER CHICKEN DINNER.\nJE HEBT DE CODE GERADEN IN {steps} STAPPEN!")
             break
