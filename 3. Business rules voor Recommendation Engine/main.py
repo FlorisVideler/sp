a46f8676d3e060cf0c72dd99ed6e2d72ae8bcae8
Floris Videler <floris.videler@student.hu.nl>
2020-03-19 09:56:15 +0100

    Added connection to the database

--- 3. Business rules voor Recommendation Engine/main.py
+++ 3. Business rules voor Recommendation Engine/main.py
@@ -1,1 +1,18 @@
-p
+import pymongo, psycopg2, csv
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+mongoDB = client["huwebshop"]
+
+hostname = 'localhost'
+username = 'postgres'
+password = 'Floris09'
+database = 'huwebshop'
+
+conn = psycopg2.connect(host=hostname, user=username, password=password, database=database)
+cur = conn.cursor()
+
+cur.execute('''
+        DROP TABLE IF EXISTS collab_recs;
+        DROP TABLE IF EXISTS content_recs;
+        ''')
+
